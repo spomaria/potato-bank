c3b15ed88d16e9dcb1d9258f615fdd31d615a2ef
spomaria <spomariae@gmail.com>
2023-10-09 23:23:33 +0100

    Update

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -10,6 +10,8 @@ const Services = () => {
   // setting up function to enable pop up forms
   const [buttonPopup, setButtonPopup] = useState(false);
   const [invoice, setInvoice] = useState(false);
+  const [selected, setSelected] = useState("");
+  const [choice, setChoice] = useState("");
 
   return (
     <div>
@@ -22,8 +24,8 @@ const Services = () => {
             <h3>Order Form</h3>
             <br />
             <form>
-              Potato Variety <input type="text" /> <br /> <br />
-              Category <input type="text" /> <br /> <br />
+              <PotatoVariety selected ={selected} setSelected ={setSelected}/>
+              <PotatoCategory choice ={choice} setChoice ={setChoice}/>  
               Price/Kg <input type="text" /> <br /> <br />
               Quantity (Kg) <input type="text" placeholder='please input a number'/> <br /> <br />
               Email Address: <input type='email' placeholder='youremail@email.com'/> <br /><br />
@@ -31,7 +33,7 @@ const Services = () => {
               Preffered Pick Up Date <input type="date" /> <br /> <br />
 
             </form>
-            <button>Proceed to Pay</button>
+            <button className='proceed'>Proceed to Pay</button>
           </OrderForm>
         </div>
         <div className="sell-potato">
@@ -42,8 +44,8 @@ const Services = () => {
             <h3>Generate Invoice</h3>
             <br />
             <form>
-              Potato Variety <input type="text" /> <br /> <br />
-              Category <input type="text" /> <br /> <br />
+              <PotatoVariety selected ={selected} setSelected ={setSelected}/>
+              <PotatoCategory choice ={choice} setChoice ={setChoice}/>  
               Price/Kg <input type="text" /> <br /> <br />
               Quantity (Kg) <input type="text" placeholder='please input a number'/> <br /> <br />
               Email Address: <input type='email' placeholder='youremail@email.com'/> <br /><br />
@@ -51,7 +53,7 @@ const Services = () => {
               Proposed Transaction Date <input type="date" /> <br /> <br />
               Farm Location <input type="text" placeholder='Village and LGA'/> <br /> <br />
             </form>
-            <button>Set Payment Method</button>
+            <button className='proceed'>Set Payment Method</button>
           </Invoice>
         </div>
 
@@ -59,6 +61,66 @@ const Services = () => {
       <Footer/>
     </div>
   )
+}
+
+// This function enables a dropdown menu
+// for the potato variety
+function PotatoVariety({selected, setSelected}){
+    const [isActive, setIsActive] = useState(false);
+    const varieties = ["Caruso", "Nicola",
+      "Marabel", "Connect", "Lady Christyl"];
+
+    return(
+      <div className="dropdown">
+        <div className="dropdown-btn" onClick={(e) => 
+          setIsActive(!isActive)}>
+            {selected}
+            <span className='fas fa-caret-down'></span>
+        </div>
+        {isActive && (
+          <div className="dropdown-content">
+            {varieties.map((variety) => (
+              <div onClick={(e) =>{
+                  setSelected(variety);
+                  setIsActive(false);
+                }} className="dropdown-item">
+                  {variety}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+}
+
+// This function enables a dropdown menu
+// for the potato category by size
+function PotatoCategory({choice, setChoice}){
+  const [isChosen, setIsChosen] = useState(false);
+  const options = ["Small Size", 
+    "Midium Size", "Table Size"];
+
+  return(
+    <div className="dropdown">
+      <div className="dropdown-btn" onClick={(e) => 
+        setIsChosen(!isChosen)}>
+          {choice}
+          <span className='fas fa-caret-down'></span>
+      </div>
+      {isChosen && (
+        <div className="dropdown-content">
+          {options.map((option) => (
+            <div onClick={(e) =>{
+                setChoice(option);
+                setIsChosen(false);
+              }} className="dropdown-item">
+                {option}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Services;
